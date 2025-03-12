ac6a9d094df9dda3c5afb55160d227d21ceb2a28
Sherry Wu <shw18731@gmail.com>
2025-03-12 23:56:09 +0800

    fix bug

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,7 +7,7 @@ function LoginPage({ setIsAuth }) {
   
   const [account, setAccount] = useState({
     username: "shw18731@gmail.com",
-    password: "qaz123wsx456",
+    password: "",
   });
   const handleInputChange = (e) => {
     const { value, name } = e.target;
